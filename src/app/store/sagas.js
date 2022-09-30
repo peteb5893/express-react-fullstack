a566c5d8b11767a7f5becfe31ea00a0a9c40428e
Peter Bennington <pbennington@appliedsystems.com>
2022-09-30 17:01:14 +0100

    create the real sagas to interact with db

---
+++ src/app/store/sagas.js
@@ -0,0 +1,43 @@
+import { take, put, select } from "redux-saga/effects";
+import * as mutations from "./mutations";
+import { v4 as uuid } from "uuid";
+import axios from "axios";
+
+const url = "http://localhost:7777";
+
+export function* taskCreationSaga() {
+  while (true) {
+    const { groupID } = yield take(mutations.REQUEST_TASK_CREATION);
+    const ownerID = "U1";
+    const taskID = uuid();
+    yield put(mutations.createTask(taskID, groupID, ownerID));
+    const { res } = yield axios.post(url + "/task/new", {
+      task: {
+        id: taskID,
+        group: groupID,
+        owner: ownerID,
+        isComplete: false,
+        name: "New Task",
+      },
+    });
+  }
+}
+
+export function* taskModificationSaga() {
+  while (true) {
+    const task = yield take([
+      mutations.SET_TASK_COMPLETE,
+      mutations.SET_TASK_GROUP,
+      mutations.SET_TASK_NAME,
+    ]);
+
+    axios.post(url + "/task/update", {
+      task: {
+        id: task.taskID,
+        group: task.groupID,
+        name: task.name,
+        isComplete: task.isComplete,
+      },
+    });
+  }
+}
